5b3d47cdecbebbd4c08d9ddd22c1a349d47668ce
sabbirahmedseam <sabbirahmedseam1@gmail.com>
2022-11-08 12:45:18 +0600

    send

--- index.js
+++ index.js
@@ -6,8 +6,6 @@ app.use(express.json());
 app.use(cors());
 require("dotenv").config();
 
-
-
 const { MongoClient, ServerApiVersion } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.qzdbt4w.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
@@ -18,10 +16,18 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     const userCollection = client.db("photography").collection("user");
-    app.get("/item", async (req, res) => {
+    app.get("/items", async (req, res) => {
+      const query = {};
+      const cursor = userCollection.find(query).limit(3);
+      const result = await cursor.toArray();
+      // console.log(result);
+      res.send(result);
+    });
+    app.get("/allcatg", async (req, res) => {
       const query = {};
       const cursor = userCollection.find(query);
       const result = await cursor.toArray();
+      // console.log(result);
       res.send(result);
     });
 
